82abbee517c2fa2b6373af3a588bef933c754413
Jeremy Lainé <jeremy.laine@m4x.org>
2012-12-11 08:50:07 +0100

    squash migrations

--- timegraph/migrations/0001_initial.py
+++ timegraph/migrations/0001_initial.py
@@ -1,17 +1,17 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 import datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
 
+
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
         # Adding model 'Graph'
         db.create_table('timegraph_graph', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50, db_index=True)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50)),
             ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('lower_limit', self.gf('django.db.models.fields.IntegerField')(default=0, null=True, blank=True)),
             ('upper_limit', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
@@ -34,17 +34,15 @@ class Migration(SchemaMigration):
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=32)),
             ('parameter', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('type', self.gf('django.db.models.fields.CharField')(default='float', max_length=16)),
             ('unit', self.gf('django.db.models.fields.CharField')(max_length=6, blank=True)),
             ('rrd_enabled', self.gf('django.db.models.fields.BooleanField')(default=True)),
             ('graph_color', self.gf('django.db.models.fields.CharField')(max_length=8, blank=True)),
-            ('cumulative', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('device', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
         db.send_create_signal('timegraph', ['Metric'])
 
 
     def backwards(self, orm):
-        
         # Deleting model 'Graph'
         db.delete_table('timegraph_graph')
 
@@ -63,20 +61,19 @@ class Migration(SchemaMigration):
             'is_visible': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'lower_limit': ('django.db.models.fields.IntegerField', [], {'default': '0', 'null': 'True', 'blank': 'True'}),
             'metrics': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['timegraph.Metric']", 'symmetrical': 'False'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'type': ('django.db.models.fields.CharField', [], {'default': "'LINE'", 'max_length': '255'}),
             'upper_limit': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
         },
         'timegraph.metric': {
             'Meta': {'ordering': "['name']", 'object_name': 'Metric'},
-            'cumulative': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'device': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'graph_color': ('django.db.models.fields.CharField', [], {'max_length': '8', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
             'parameter': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
             'rrd_enabled': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'default': "'float'", 'max_length': '16'}),
             'unit': ('django.db.models.fields.CharField', [], {'max_length': '6', 'blank': 'True'})
         }
     }
